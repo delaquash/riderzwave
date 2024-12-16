16f84fd37273c69ba6bb04aa06c250dd1e16a29b
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-12-16 12:38:03 +0100

    add email otp route

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { registerUser,verifyOtp, signUpNewUser } from "../controllers/UserController";
+import { registerUser,verifyOtp, signUpNewUser, sendOtpToMail } from "../controllers/UserController";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/verifyOtp", verifyOtp);
 router.put("/signup", signUpNewUser)
+router.put("/email-otp-verification", sendOtpToMail)
 
 export default router;

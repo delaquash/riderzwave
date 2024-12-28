03a7c861ef1e760ce138ac5dbeb63e2cb8966370
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-12-28 16:43:08 +0100

    fix: package update

---
+++ driver/app/(routes)/signup/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import SignUp from '@/screen/signup/SignUp'
+
+export default function index() {
+  return (
+    <SignUp />
+  )
+}
+

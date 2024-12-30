4553fafd5eedbf9d1cdfc35067c63b60c978fd9b
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2024-12-30 18:27:57 +0100

    feat: add state for document verification

--- driver/screen/document-verification/DocumentVerification.tsx
+++ driver/screen/document-verification/DocumentVerification.tsx
@@ -1,9 +1,21 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { windowHeight } from '@/themes/app.constant'
+import { windowHeight, windowWidth } from '@/themes/app.constant'
 import ProgressBar from '@/components/common/ProgressBar'
+import TitleView from '@/components/TitleView/TitleView'
+import style from "../signup/style";
+import color from '@/themes/app.colors'
+import Input from '@/components/common/input'
+import SelectInput from '@/components/SelectInput/SelectInput'
+import Button from '@/components/common/button'
+import { router, useLocalSearchParams } from 'expo-router'
+import axios from 'axios'
+import { Toast } from 'react-native-toast-notifications'
+import { useTheme } from '@react-navigation/native'
 
 const DocumentVerification = () => {
+  const { colors } = useTheme();
+  
   return (
     <ScrollView>
       <View>
@@ -18,8 +30,30 @@ const DocumentVerification = () => {
         >
           Ride Wave
         </Text>
-        <View>
-            <ProgressBar fill={1}/>
+        <View
+          style={{
+            padding: windowWidth(20),
+          }}
+        >
+            <ProgressBar fill={1} />
+            <View
+              style={[style.subView, { backgroundColor: colors.background}]}
+            >
+              <View
+                style={style.space}
+              >
+                <TitleView 
+                  title={"Vehicle Registration"}
+                  subTitle={"Explore a seemless journey by joining Ride Wave"}
+                />
+                <SelectInput 
+                  title='Vehicle Type'
+                  placeholder='Choose your vehicle type'
+                  value={}
+                
+                />
+              </View>
+            </View>
         </View>
       </View>
     </ScrollView>

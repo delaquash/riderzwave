a5b31ecb41d24f92cda3430c3b28abbd84a1db8a
Olaide Emmanuel <olaidemmanuel0@gmail.com>
2025-01-03 13:19:42 +0100

    feat:(header) add header to driver homescreen

--- driver/screen/home/HomeScreen.tsx
+++ driver/screen/home/HomeScreen.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Header from '@/components/common/Header'
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <View>
+        <Header />
+      </View>
     </View>
   )
 }
